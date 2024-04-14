7ea66d7fb8571ef64b6812422a87b9593b71e9be
COUDRA <112389542+COUDR@users.noreply.github.com>
2024-04-14 03:23:19 +0200

    Add files via upload

--- ZeMusic/__init__.py
+++ ZeMusic/__init__.py
@@ -1,26 +1,7 @@
-from ZeMusic.core.bot import Mody
-from ZeMusic.core.dir import dirr
-from ZeMusic.core.git import git
-from ZeMusic.core.userbot import Userbot
-from ZeMusic.misc import dbb, heroku
-
-from .logging import LOGGER
-
-dirr()
-git()
-dbb()
-heroku()
-
-app = Mody()
-userbot = Userbot()
-
-
-from .platforms import *
-
-Apple = AppleAPI()
-Carbon = CarbonAPI()
-SoundCloud = SoundAPI()
-Spotify = SpotifyAPI()
-Resso = RessoAPI()
-Telegram = TeleAPI()
-YouTube = YouTubeAPI()
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
