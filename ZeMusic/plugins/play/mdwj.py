f4ba5f38781863abf0bb4ce4f537918be2880397
COUDRA <112389542+COUDR@users.noreply.github.com>
2024-04-15 06:31:59 +0200

    Update mdwj.py

--- ZeMusic/plugins/play/mdwj.py
+++ ZeMusic/plugins/play/mdwj.py
@@ -140,7 +140,7 @@ async def ginnj(client: Client, message: Message):
     chat_name = message.chat.title
     chat_username = f"@{message.chat.username}"
     photo = await client.download_media(message.chat.photo.big_file_id)
-    await message.reply_photo(photo=photo, caption=f"""[🐲 ¦ الاسم ] {chat_name}\n🚸 ¦[ ايدي الجروب ]  [-{chat_idd}\n🐊 ¦[ رابط ] {chat_username}**""",     
+    await message.reply_photo(photo=photo, caption=f"""🐲 ¦ الاسم  {chat_name}\n🚸 ¦ ايدي الجروب   -{chat_idd}\n🐊 ¦ رابط  {chat_username}**""",     
     reply_markup=InlineKeyboardMarkup(
             [
                 [
