4cc7a26213a35b779db72c9cf88911c3ce02e545
COUDRA <112389542+COUDR@users.noreply.github.com>
2024-04-15 06:51:45 +0200

    Update hmsa.py

--- ZeMusic/plugins/play/hmsa.py
+++ ZeMusic/plugins/play/hmsa.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import os
-from AFROTOMusic import app
+from ZeMusic import app
 from config import *
 
 
