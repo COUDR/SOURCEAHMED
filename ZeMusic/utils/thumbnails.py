f36f58949081d810c1134c9bb376852ad48f220a
COUDRA <112389542+COUDR@users.noreply.github.com>
2024-04-17 05:43:04 +0200

    Update thumbnails.py

--- ZeMusic/utils/thumbnails.py
+++ ZeMusic/utils/thumbnails.py
@@ -8,7 +8,7 @@ import aiofiles
 import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
 from youtubesearchpython.__future__ import VideosSearch
-import numpy as np
+
 
 from config import YOUTUBE_IMG_URL
 
